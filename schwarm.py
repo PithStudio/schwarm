7d21d8258fa931acf7824e2069849a3189c7889f
amng <arnmng@gmail.com>
2013-03-13 16:01:51 -0400

    Base logic for data file is done along with iteration

---
+++ schwarm.py
@@ -0,0 +1,10 @@
+import numpy
+import scipy
+import matplotlib
+from numpy import ndarray
+
+print(
+    numpy.array([
+        [1.2, 3.4, 6.7],
+        [84.3, 29.3, 38.1]],
+    numpy.dtype(float)))
